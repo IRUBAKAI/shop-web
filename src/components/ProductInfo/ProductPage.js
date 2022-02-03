65b2676c8d3f95ecd8c60fad550cf1cc977d9516
IRUBAKAI <65865941+IRUBAKAI@users.noreply.github.com>
2022-02-03 15:11:25 +0200

    Update ProductPage.js

--- src/components/ProductInfo/ProductPage.js
+++ src/components/ProductInfo/ProductPage.js
@@ -16,7 +16,7 @@ class ProductPage extends PureComponent {
     let id = window.location.href.slice(39);
     const fetchDataId = async () => {
       const queryResult = await axios.post(GRAPHQL_API, {
-        query: GET_ID(window.location.href.slice(39)),
+        query: GET_ID(id),
       });
 
       const result = queryResult.data.data;
